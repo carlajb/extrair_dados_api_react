47446487ad35572ac9f314af90844c8835068bfd
carlajb <carla_agencia@hotmail.com>
2021-02-26 23:17:12 -0300

    extrair dados api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from 'react';
+import api from './api';
+
+
+class App extends Component{
+
+ state={
+   cadastros:[],
+ }
+
+ async componentDidMount(){
+   const response = await api.get('mediadores');
+
+   //console.log(response.data);
+
+   this.setState({ cadastros: response.data});
+ }
+
+
+  render(){
+
+    const {cadastros}= this.state;
+
+    return(
+      <div>
+<h2>visitar registros de cadastro</h2>
+
+    {console.log(cadastros)}
+    {cadastros.map(cadastro =>(
+      <li key={cadastro._id}>
+       
+          <h4>Nome do mediador:</h4>   {cadastro.nome}
+        
+         
+          <h4>Email: </h4>
+          {cadastro.email}
+          
+          <p>identificacao (id)</p>
+          <p>
+          {cadastro._id}
+          </p>
+       
+
+      </li>
+    ))}
     </div>
-  );
-}
+    
+    );
+  };
+};
+
+
 
 export default App;
